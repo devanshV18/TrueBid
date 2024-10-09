b25da3f4ce016b4d810ee6f9a87adac07f554ad9
devanshV18 <devanshverma024@gmail.com>
2024-10-10 01:00:15 +0530

    fixed and ceased the access create auctions for bidder role users.

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -26,3 +26,12 @@ export const isAuthenticated = catchAsyncErrors(async(req,res,next) => {
     req.user = await User.findById(decoded.id)
     next()
 })
+
+export const isAuthorized = (...roles) => {
+    return(req,res,next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`${req.user.role} not allowed to access this resource.`, 403))
+        }
+        next()
+    }
+}
--- server/routes/auctionItemRoutes.js
+++ server/routes/auctionItemRoutes.js
@@ -1,9 +1,9 @@
 import { addNewAuctionItem } from "../controllers/auctionItemController.js";
-import { isAuthenticated } from "../middlewares/auth.js";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 import express from "express";
 
 const router = express.Router()
 
-router.post("/create",isAuthenticated, addNewAuctionItem)
+router.post("/create",isAuthenticated, isAuthorized("Auctioneer"), addNewAuctionItem)
 
 export default router
