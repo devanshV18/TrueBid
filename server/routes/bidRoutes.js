12637f0f27639631027db4e76fe4360eb35dd3e9
devanshV18 <devanshverma024@gmail.com>
2024-10-10 21:52:04 +0530

    completed bid ROutes (placeBid function) and also integrated it with our routes. Mongo DB connection issue fixed.

--- server/routes/bidRoutes.js
+++ server/routes/bidRoutes.js
@@ -1,9 +1,10 @@
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 import express from "express";
 import { placeBid } from "../controllers/bidController.js";
+import { checkAuctionEndTime } from "../middlewares/checkAuctionEndTime.js";
 
 const router = express.Router()
 
-router.post("/place/:id", isAuthenticated, isAuthorized("Bidder"), placeBid)
+router.post("/place/:id", isAuthenticated, isAuthorized("Bidder"), checkAuctionEndTime, placeBid)
 
 export default router
