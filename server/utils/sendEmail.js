274ff83732df55b3619b78acdf67683f3f172e02
devanshV18 <devanshverma024@gmail.com>
2024-10-16 00:06:12 +0530

    setup of nodemailer and config dependencies done.

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,26 @@
+import nodeMailer from "nodemailer"
+
+export const sendEmail = async( {emailTo, subject, message} ) => {
+    
+    //SETTING UP OUR TRANSPORTER
+    const transporter = nodeMailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        service: process.env.SMTP_SERVICE,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
+        }
+    })
+
+
+    //configuring the sender,reciever, message and subject
+    const options = {
+        from: process.env.SMTP_MAIL,
+        to: emailTo,
+        subject: subject,
+        text: message
+    }
+
+    await transporter.sendMail(options)
+}
