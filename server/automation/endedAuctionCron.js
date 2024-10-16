c26572711e2f1c4785560331220e9de38b8dc1ef
devanshV18 <devanshverma024@gmail.com>
2024-10-16 16:42:44 +0530

    sendEmail function ebugged and works fine.

--- server/automation/endedAuctionCron.js
+++ server/automation/endedAuctionCron.js
@@ -1,71 +1,88 @@
-import cron from "node-cron"
-import {Auction} from "../models/auctionSchema.js"
-import {User} from "../models/userSchema.js"
-import { calculateCommission } from "../controllers/commissionController.js"
-import { Bid } from "../models/bidSchema.js"
-import {sendEmail} from "../utils/sendEmail.js"
+import cron from "node-cron";
+import { Auction } from "../models/auctionSchema.js";
+import { User } from "../models/userSchema.js";
+import { Bid } from "../models/bidSchema.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { calculateCommission } from "../controllers/commissionController.js";
 
 export const endedAuctionCron = () => {
-    // min hr days month year -> star 
+  cron.schedule("*/1 * * * *", async () => {
+    const now = new Date();
+    console.log("Cron for ended auction running...");
 
-    cron.schedule("*/1 * * * *", async() => {
-        const now = new Date()
-        // console.log("Cron for ended auction running")
+    const endedAuctions = await Auction.find({
+      endTime: { $lt: now },
+      commissionCalculated: false,
+    });
 
-        //fetching the ended auctions -> auctions that have end time before date.now and its commission is not yeat calculated -> false
-        const endedAuctions = await Auction.find({
-            endTime: { $lt: now },
-            commissionCalculated: false
-        })
-        for(const auction of endedAuctions){
-            try {
-                const commissionAmount = calculateCommission(auction._id)
-                auction.commissionCalculated = true
-                const highestBidder = await Bid.findOne({
-                    auctionItem: auction._id,
-                    amount: auction.currentBid
-                })
-                const auctioneer = await User.findById(auction.createdBy)
-                auctioneer.unpaidCommision = commissionAmount
 
-                if(highestBidder){
-                    auctioneer.highestBidder = highestBidder.bidder.id
-                    await auction.save()
-                    const bidder = await User.findById(highestBidder.bidder.id)
-                    const highestBidAmount = bidder.moneySpent + highestBidder.amount
+    for (const auction of endedAuctions) {
 
-                    await User.findByIdAndUpdate(
-                        bidder._id, 
-                        {
-                        $inc: {
-                            moneySpent: highestBidAmount,
-                            auctionsWon: 1
-                        },
-                        },
-                        { new: true }
-                    )
+      try {
 
-                    await User.findByIdAndUpdate(auctioneer._id, {
-                        $inc: {
-                            unpaidCommision: commissionAmount
-                        },
-                    },
-                    { new: true }
-                )
+        const commissionAmount = await calculateCommission(auction._id);
+        auction.commissionCalculated = true;
 
-                    const subject = `Congratulations! You won the auction for ${auction.title}`
-                    const message = `Dear ${bidder.userName}, \n\nCongratulations! You have won the auction for ${auction.title}. \n\nBefore proceeding for payment contact your auctioneer via your auctioneer email:${auctioneer.email} \n\nPlease complete your payment using one of the following methods:\n\n1. **Bank Transfer**: \n- Account Name: ${auctioneer.paymentMethods.bankTransfer.AccountHandlerName} \n- Account Number: ${auctioneer.paymentMethods.bankTransfer.bankAccountNumber} \n- Bank: ${auctioneer.paymentMethods.bankTransfer.bankName}\n\n2. **GooglePay**:\n- You can send payment via GooglePay: ${auctioneer.paymentMethods.upi.upiId}\n\n3.  **Cash on Delivery (COD)**:\n- If you prefer COD, you must pay 20% of the total amount upfront before delivery.\n- To pay the 20% upfront, use any of the above methods.\n- The remaining 80% will be paid upon delivery.\n- If you want to see the condition of your auction item then send your email on this: ${auctioneer.email}\n\nPlease ensure your payment is completed by [Payment Due Date]. Once we confirm the payment, the item will be shipped to you.\n\nThank you for participating!\n\nBest regards,\nTrueBid auctions, India`;
+        const highestBidder = await Bid.findOne({
+          auctionItem: auction._id,
+          amount: auction.currentBid,
+        });
 
-                    console.log("Sending Email to the highest Bidder")
-                    sendEmail( { emailTo: bidder.email, subject: subject, message: message } )
-                    console.log("The email was successfully sent!")
+        const auctioneer = await User.findById(auction.createdBy);
 
-                }else{
-                    await auction.save()
-                }
-            } catch (error) {
-                return next(console.log(error || "Something went wrong with the ended auction cron"))
-            }
+        auctioneer.unpaidCommision = commissionAmount;
+
+        if (highestBidder) {
+
+          auction.highestBidder = highestBidder.bidder.id;
+
+          await auction.save();
+
+          const bidder = await User.findById(highestBidder.bidder.id);
+
+          console.log(bidder.email)
+
+          await User.findByIdAndUpdate(
+            bidder._id,
+            {
+              $inc: {
+                moneySpent: highestBidder.amount,
+                auctionsWon: 1,
+              },
+            },
+            { new: true }
+          );
+
+          await User.findByIdAndUpdate(
+            auctioneer._id,
+            {
+              $inc: {
+                unpaidCommission: commissionAmount,
+              },
+            },
+            { new: true }
+          );
+
+          const subject = `Congratulations! You won the auction for ${auction.title}`;
+
+          const message = `Dear ${bidder.userName}, \n\nCongratulations! You have won the auction for ${auction.title}. \n\nBefore proceeding for payment contact your auctioneer via your auctioneer email:${auctioneer.email} \n\nPlease complete your payment using one of the following methods:\n\n1. **Bank Transfer**: \n- Account Handler Name: ${auctioneer.paymentMethods.bankTransfer.AccountHandlerName} \n- Account Number: ${auctioneer.paymentMethods.bankTransfer.bankAccountNumber} \n- Bank: ${auctioneer.paymentMethods.bankTransfer.bankName}\n\n2. **GooglePay**:\n- Send payment to: ${auctioneer.paymentMethods.upi.upiId}\n\n3. **Cash on Delivery (COD)**:\n- If you prefer COD, you must pay 20% of the total amount upfront before delivery.\n- To pay the 20% upfront, use any of the above methods.\n- The remaining 80% will be paid upon delivery.\n- If you want to see the condition of your auction item then send your email on this: ${auctioneer.email}\n\nPlease ensure your payment is completed by [Payment Due Date]. Once we confirm the payment, the item will be shipped to you.\n\nThank you for participating!\n\nBest regards,\nTrueBid Auctions, India`;
+
+         
+          console.log("SENDING EMAIL TO HIGHEST BIDDER");
+
+          sendEmail({ emailTo: bidder.email, subject, message });
+
+          console.log("SUCCESSFULLY EMAIL SEND TO HIGHEST BIDDER");
+        } else {
+
+          await auction.save();
+
         }
-    })
-}
+      } catch (error) {
+
+        return next(console.error(error || "Some error in ended auction cron"));
+
+      }
+    }
+  });
+};
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,6 +7,7 @@ export const sendEmail = async( {emailTo, subject, message} ) => {
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
         service: process.env.SMTP_SERVICE,
+        secure: true,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
