0547e2ce50ce884e469a3d8d264ec19ef06774d4
devanshV18 <devanshverma024@gmail.com>
2024-10-17 01:46:13 +0530

    initiated redux reducers -> understanding it briefly and its easy tbh

--- client/src/store/slices/userSlice.js
+++ client/src/store/slices/userSlice.js
@@ -12,6 +12,21 @@ const userSlice = createSlice({
         error: null
     },
     reducers: {
+        registerRequest(state,action){
+            state.loading = true;
+            state.isAuthenticated = false,
+            state.user = {}
+        },
+        registerSuccess(state,action){
+            state.loading = false;
+            state.isAuthenticated = true,
+            state.user = action.payload.user
+        },
+        registerFailed(state,action){
+            state.loading = false;
+            state.isAuthenticated = false,
+            state.user = {}
+        },
         logoutSuccess(state, action){
             state.isAuthenticated = false
             state.user = {}
@@ -29,6 +44,32 @@ const userSlice = createSlice({
         }
     }
 })
+
+export const register = (data) => async (dispatch) => {
+    dispatch(userSlice.actions.registerRequest())
+    try {
+        const response = await axios.post
+        (
+            "http://localhost:5000/api/users/register", 
+            data, 
+            {
+                withCredentials: true,
+                headers: { "Content-Type": "multipart/form-data" }
+            }
+        )
+
+        dispatch(userSlice.actions.registerSuccess(response.data))
+        toast.success(response.data.message)
+        dispatch(userSlice.actions.clearAllErrors())
+
+    } catch (error) {
+
+        dispatch(userSlice.actions.registerFailed())
+        toast.error(error.response.data.message)
+        dispatch(userSlice.actions.clearAllErrors())
+
+    }
+}
 
 export const logout = () => async(dispatch) => {
     try {
