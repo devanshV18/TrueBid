875ea2fae922286bec26aafe5aef6a635b57cb00
devanshV18 <devanshverma024@gmail.com>
2024-10-18 21:37:24 +0530

    settled up commission slice and configured it.

---
+++ client/src/pages/SubmitCommission.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubmitCommission = () => {
+  return (
+    <div>
+      Commission
+    </div>
+  )
+}
+
+export default SubmitCommission
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./slices/userSlice"
+import commissionReducer from "./slices/commissionSlice"
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        commission: commissionReducer
     }
 })
