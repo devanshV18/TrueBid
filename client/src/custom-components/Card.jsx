784404692b7b5df10acd94a26fcf5734b1722f3d
devanshV18 <devanshverma024@gmail.com>
2024-10-19 15:52:06 +0530

    card done

--- client/src/custom-components/Card.jsx
+++ client/src/custom-components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const Card = ({ imgSrc, title, startingBid, startTime, endTime, id }) => {
   const calculateTimeLeft = () => {
     const now = new Date();
@@ -43,40 +44,68 @@ const Card = ({ imgSrc, title, startingBid, startTime, endTime, id }) => {
   };
 
   return (
-    <>
-      <Link
-        to={`/auction/item/${id}`}
-        className="flex-grow basis-full bg-white rounded-md group sm:basis-56 lg:basis-60 2xl:basis-80"
-      >
-        <img
-          src={imgSrc}
-          alt={title}
-          className="w-full aspect-[4/3] m-auto md:p-12"
-        />
-        <div className="px-2 pt-4 pb-2">
-          <h5 className="font-semibold text-[18px] group-hover:text-[#d6482b] mb-2">
-            {title}
-          </h5>
-          {startingBid && (
-            <p className="text-stone-600 font-light">
-              Starting Bid:{" "}
-              <span className="text-[#fdba88] font-bold ml-1">
-                {startingBid}
-              </span>
-            </p>
-          )}
-          <p className="text-stone-600 font-light">
-            {timeLeft.type}
-            {Object.keys(timeLeft).length > 1 ? (
-              <span className="text-[#fdba88] font-bold ml-1">
-                {formatTimeLeft(timeLeft)}
-              </span>
-            ) : (
-              <span className="text-[#fdba88] font-bold ml-1">Time's up!</span>
-            )}
-          </p>
-        </div>
-      </Link>
+  <>
+    <div className="bg-white transform transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-black max-w-sm h-100 border border-gray-300 rounded-md">
+
+        <Link
+          to={`/auction/item/${id}`}
+          className="flex-grow basis-full bg-white rounded-xl group sm:basis-56 lg:basis-64 2xl:basis-72 h-full"
+        >
+              <img
+                src={imgSrc}
+                alt={title}
+                className="w-full aspect-[4/3] m-auto md:p-10 transform transition-transform duration-300 ease-in-out group-hover:scale-110"
+              />
+
+
+            <div className="px-2 pt-4 pb-2">
+
+                <h5 className="font-semibold text-xl group-hover:text-black mb-2 p-2">
+                  {title} hello
+                </h5>
+
+
+                {startingBid && (
+                  <p className="text-stone-600 font-light">
+s
+                    Starting Bid:{" "}
+
+                    <span className="text-[#72a24d] font-bold ml-1">
+                      {startingBid}
+                    </span>
+
+                  </p>
+                )}
+
+
+                <p className="text-stone-600 font-light mb-6">
+
+                  {timeLeft.type}
+
+                  {  Object.keys(timeLeft).length > 1 ? 
+                      (
+
+                        <span className="text-black font-bold ml-1">
+                          {formatTimeLeft(timeLeft)}
+                        </span>
+
+                      ) 
+                      : 
+                      (
+                        <span className="text-[#72a24d] text-xl font-bold mb-2">Time's up!</span>
+                      )
+                  }
+
+                </p>
+
+            </div>
+
+        </Link>
+    </div>
+
+
+
+
     </>
   );
 };
