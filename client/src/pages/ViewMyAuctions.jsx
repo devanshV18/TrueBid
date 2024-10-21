94b82e6bcf8f72a1aef23564d65753ab65096fc5
devanshV18 <devanshverma024@gmail.com>
2024-10-21 23:00:37 +0530

    my auctions page done

--- client/src/pages/ViewMyAuctions.jsx
+++ client/src/pages/ViewMyAuctions.jsx
@@ -25,30 +25,36 @@ const ViewMyAuctions = () => {
         <h1 className={`text-[#72a24d] text-2xl font-bold mb-2 min-[480px]:text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl`}>My Auctions
         </h1>
 
-        {
-            loading ? <Spinner/> 
-            : 
-            <div className='flex flex-wrap gap-6'>
-
-                { 
-                    myAuctions.map((element) => {
-                    return (
-                        <CardTwo
-                            title={element.title}
-                            startingBid={element.startingBid}
-                            endTime={element.endTime}
-                            startTime={element.startTime}
-                            imgSrc={element.image?.url}
-                            id={element._id}
-                            key={element._id}
-                        />
-                        );
-                    }
-                    )
-                }   
-
-            </div>
-        }
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div
+            className={`${
+              myAuctions.length > 2 && "flex-grow"
+            } flex flex-wrap gap-6`}
+          >
+            {myAuctions.length > 0 ? (
+              myAuctions.map((element) => {
+                return (
+                  <CardTwo
+                    title={element.title}
+                    startingBid={element.startingBid}
+                    endTime={element.endTime}
+                    startTime={element.startTime}
+                    imgSrc={element.image?.url}
+                    id={element._id}
+                    key={element._id}
+                  />
+                );
+              })
+            ) : (
+              <h3 className="text-[#666] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl mt-5">
+                You have not posted any auction.
+              </h3>
+            )}{" "}
+            :
+          </div>
+        )}
     </div>
   )
 }
