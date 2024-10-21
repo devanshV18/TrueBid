099cfb3224101180383cc8ead4fd57de0c073716
devanshV18 <devanshverma024@gmail.com>
2024-10-21 23:57:01 +0530

    settled up admin slice in store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,12 +3,14 @@ import userReducer from "./slices/userSlice"
 import commissionReducer from "./slices/commissionSlice"
 import auctionReducer from "./slices/auctionSlice"
 import bidReducer from "./slices/bidSlice"
+import adminReducer from "./slices/adminSlice"
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         commission: commissionReducer,
         auction: auctionReducer,
-        bid: bidReducer
+        bid: bidReducer,
+        admin: adminReducer
     }
 })
